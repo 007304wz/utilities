20303cfe9e9ee05e4bb2f4514b5a0b68ec776613
007304wz <007304wz@gmail.com>
2019-11-12 13:28:50 -0800

    1st version

---
+++ src/deepClone.js
@@ -0,0 +1,29 @@
+function customCloneDeep(obj) {
+
+    if (['number', 'string', 'boolean'].indexOf(typeof obj) !== -1) {
+        return obj;
+    }
+    if (obj instanceof Date) {
+        return new Date().setTime(obj.getTime());
+    }
+    if (obj instanceof Object && !(obj instanceof Array) && !(obj instanceof Function) ) {
+        const copy = new obj.constructor() || {};
+        for (const attribute in obj) {
+            if (obj.hasOwnProperty(attribute)) {
+                copy[attribute] = obj[attribute] ? customCloneDeep(obj[attribute]) : obj[attribute];
+            }
+        }
+        return copy;
+    }
+    if (obj instanceof Array) {
+        const copy = [];
+        obj.forEach(item => {
+            copy.push(customCloneDeep(item));
+        });
+        return copy;
+    }
+    if (obj instanceof Function) {
+        return obj;
+    }
+
+} 
